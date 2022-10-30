eef1e6767953183a71fee4082ff016e220c4edb4
fathularif <fathulcode@gmail.com>
2022-10-30 23:20:37 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="text-center">Hellow There</div>;
+}
+
+export default App;
